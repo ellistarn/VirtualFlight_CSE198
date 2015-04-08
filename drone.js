25e99e2cc065b6d40d58570cbe06592bacb8bab0
Ellis Bigelow <ellisbigelow@gmail.com>
2015-04-07 17:30:51 -0700

    created value checking for oculus commands

--- drone.js
+++ drone.js
@@ -4,25 +4,54 @@
 MAXROLL = 30;
 MINROLL = 30;
 
+MAXTILT = 30;
+MINTILT = 30;
 
+MAXYAW = 30;
+MINYAW = 30;
 
-//takes a value from 
+//takes a value between MINROLL & MAXROLL and sends command to drone
 function roll(degrees) {   
     if (degrees > MAXROLL) {
         degrees = MAXROLL;
+        console.log("WARNING: limiting roll from {0} to {1}".format(degrees,MAXROLL))
     }
     else if (degrees < MINROLL) {
         degrees = MINROLL;
+        console.log("WARNING: limiting roll from {0} to {1}".format(degrees,MINROLL))
     }
+    console.log("CMD: roll {0}deg".format(degree))
+    //TODO call roll
 }
 
+//takes a value between MINTILT & MAXTILT and sends command to drone
 function tilt(degrees) {
-
+    if (degrees > MAXTILT) {
+        degrees = MAXTILT;
+        console.log("WARNING: limiting tilt from {0} to {1}".format(degrees,MAXTILT))
+    }
+    else if (degrees < MINTILT) {
+        degrees = MINTILT;
+        console.log("WARNING: limiting tilt from {0} to {1}".format(degrees,MINTILT))
+    }
+    console.log("CMD: tilt {0}deg".format(degree))
+    //TODO call tilt
 }
 
 function yaw(degrees) {
-
+    if (degrees > MAXYAW) {
+        degrees = MAXYAW;
+        console.log("WARNING: limiting yaw from {0} to {1}".format(degrees,MAXYAW))
+    }
+    else if (degrees < MINYAW) {
+        degrees = MINYAW;
+        console.log("WARNING: limiting yaw from {0} to {1}".format(degrees,MINYAW))
+    }
+    console.log("CMD: yaw {0}deg".format(degree))
+    //TODO call yaw
 }
+
+
 
 //Ctor
 var Drone = function () {
