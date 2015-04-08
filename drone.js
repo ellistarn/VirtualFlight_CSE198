fa00215ecfee1dac837fa9b864dc9c34fff508db
Ellis Bigelow <ellisbigelow@gmail.com>
2015-04-07 18:16:46 -0700

    implemented YAWTOLERANCE

--- drone.js
+++ drone.js
@@ -1,14 +1,50 @@
 //This module serves as the main control between 
 //drove API flight commands and the Virtual Flight driver
 
-MAXROLL = 30;
-MINROLL = 30;
+//yaw must exceed this in order to generate movement commands
+YAWTOLERANCE = 10; 
 
-MAXTILT = 30;
-MINTILT = 30;
+MAXPITCH = 30;
+MINPITCH = 30;
 
 MAXYAW = 30;
 MINYAW = 30;
+
+MAXROLL = 30;
+MINROLL = 30;
+
+//takes a value between MINPITCH & MAXPITCH and sends command to drone
+function pitch(degrees) {
+    if (degrees > MAXPITCH) {
+        degrees = MAXPITCH;
+        console.log("WARNING: limiting pitch from {0} to {1}".format(degrees,MAXPITCH))
+    }
+    else if (degrees < MINPITCH) {
+        degrees = MINPITCH;
+        console.log("WARNING: limiting pitch from {0} to {1}".format(degrees,MINPITCH))
+    }
+    console.log("CMD: pitch {0}deg".format(degree))
+    //TODO call pitch
+}
+
+//takes a value between MINYAW & MAXYAW and sends command to drone.
+//must overcome YAWTOLERANCE to generate movement commands
+function yaw(degrees) {
+    if (Math.abs(degrees) < YAWTOLERANCE) {
+        return;
+    }
+
+    if (degrees > MAXYAW) {
+        degrees = MAXYAW;
+        console.log("WARNING: limiting yaw from {0} to {1}".format(degrees,MAXYAW))
+    }
+    else if (degrees < MINYAW) {
+        degrees = MINYAW;
+        console.log("WARNING: limiting yaw from {0} to {1}".format(degrees,MINYAW))
+    }
+    console.log("CMD: yaw {0}deg".format(degree))
+    //TODO call yaw
+}
 
 //takes a value between MINROLL & MAXROLL and sends command to drone
 function roll(degrees) {   
@@ -24,44 +60,15 @@ function roll(degrees) {
     //TODO call roll
 }
 
-//takes a value between MINTILT & MAXTILT and sends command to drone
-function tilt(degrees) {
-    if (degrees > MAXTILT) {
-        degrees = MAXTILT;
-        console.log("WARNING: limiting tilt from {0} to {1}".format(degrees,MAXTILT))
-    }
-    else if (degrees < MINTILT) {
-        degrees = MINTILT;
-        console.log("WARNING: limiting tilt from {0} to {1}".format(degrees,MINTILT))
-    }
-    console.log("CMD: tilt {0}deg".format(degree))
-    //TODO call tilt
-}
-
-function yaw(degrees) {
-    if (degrees > MAXYAW) {
-        degrees = MAXYAW;
-        console.log("WARNING: limiting yaw from {0} to {1}".format(degrees,MAXYAW))
-    }
-    else if (degrees < MINYAW) {
-        degrees = MINYAW;
-        console.log("WARNING: limiting yaw from {0} to {1}".format(degrees,MINYAW))
-    }
-    console.log("CMD: yaw {0}deg".format(degree))
-    //TODO call yaw
-}
-
-
-
 //Ctor
-var Drone = function () {
+var drone = function () {
     var arDrone = require('ar-drone');
     this.client = arDrone.createClient();
 
-    this.tilt = tilt;
+    this.pitch = pitch;
     this.yaw = yaw;
     this.roll = roll;
 }
 
 //Returns a client object
-module.exports = Drone;
+module.exports = drone;
