986ff5373671279e44210c20c8a682490eb29d0f
Ellis Bigelow <ellisbigelow@gmail.com>
2015-04-13 16:26:16 -0700

    creating server command api

--- VirtualFlight.js
+++ VirtualFlight.js
@@ -7,14 +7,59 @@ require("./helpers.js");
 var drone = require("./drone.js");
 
 /*
-================Stream Video================
+================Server================
 */
+var express = require('express');
+var app = express();
+
+
 var PORT = 8000;
-var PAGE = "/public/index.html"
+var PAGE = "/public/index.html";
+
+var server = app.listen(PORT, function() {
+    console.log("Started server on port %d", server.address().port);
+});
+
+
+/*
+================DRONE CONTROL HTTP API================
+*/
+app.post("/", function (req, res) {
+    var success:
+    switch(req.command) {
+        case "pitch":
+            success = drone.pitch();
+        case "yaw":
+            success = drone.yaw();
+        case "roll":
+            success = drone.roll();
+        case "ascend":
+            success = drone.ascend();
+        case "descend":
+            success = drone.descend();
+        case "hold_height":
+            success = drone.hold_height();
+        case "toggle_hover":
+            success = drone.toggle_hover();
+        case "takeoff":
+            success = drone.takeoff();
+        case "land":
+            success = drone.land();
+
+        if (!success) {
+            console.log("Post request failed")
+            res.status(500).send("command failed");
+        } else {
+            res.status(200).send("success");
+        }
+    }
+});
+
+/*
+================Video Stream================
+*/
+
 var stream = require("dronestream");
-var server = require("http").createServer(function(req, res) {
-    require("fs").createReadStream(__dirname + PAGE).pipe(res);
-}).listen(PORT);
 stream.listen(server); 
 
 
